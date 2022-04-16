711e9efd75f470c89c32f0d0295a4b8983ed4daa
SquiggelSquirrel <pjsfchaplin@hotmail.com>
2022-04-16 21:22:58 +0100

    Update SelfIntersect.gd
    Interim commit

--- SelfIntersect.gd
+++ SelfIntersect.gd
@@ -3,3 +3,7 @@ extends Object
 class_name SelfIntersect
 
 static func self_intersections(points :PoolVector2Array):
+var segments := find_segments(points)
+var collisions := []
+for i in segments.size():
+for j in range(i+1, segments.size():
